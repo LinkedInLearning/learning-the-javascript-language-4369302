23a4c3808227d488e16afa3c07645737655a2f37
Joe Chellman <joe@shooflydesign.org>
2023-01-09 13:54:20 -0800

    Added file for 02_06

---
+++ Ch02/02_06/transcript.js
@@ -0,0 +1,13 @@
+// This is a transcript of the lines of code from the lesson.
+// You can copy and paste each line into a JavaScript console to execute it and see the result.
+
+const dozen = 12, halfDozen = 6, bakersDozen = 13;
+
+dozen = 13;
+
+var cookieCount = 5;
+let cookieCount = 5;
+
+// More info:
+// http://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let
+// http://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
