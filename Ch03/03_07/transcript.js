df8bd27f281a87bf87a70c42ff636e615029cafd
Joe Chellman <joe@shooflydesign.org>
2023-01-09 16:02:29 -0800

    03_07 updated so Prettier leaves it alone

--- Ch03/03_07/transcript.js
+++ Ch03/03_07/transcript.js
@@ -1,39 +1,27 @@
-var year = 2012,
-  month = "October",
-  day = 31,
-  holiday = "Halloween";
+var year=2012,month='October',day=31,holiday='Halloween';
+
+var year   = 2012,       month    =    'October', day =          31,          holiday='Halloween';
 
 var year = 2012,
-  month = "October",
-  day = 31,
-  holiday = "Halloween";
+	month = 'October',
+	day = 31,
+	holiday = 'Halloween';
+	
+var year    = 2012,
+	month   = 'October',
+	day     = 31,
+	holiday = 'Halloween';
 
-var year = 2012,
-  month = "October",
-  day = 31,
-  holiday = "Halloween";
-
-var year = 2012,
-  month = "October",
-  day = 31,
-  holiday = "Halloween";
+var tinyAlmanac={'year':2012,'month':'October','day':31,'holiday':'Halloween'};
 
 var tinyAlmanac = {
-  year: 2012,
-  month: "October",
-  day: 31,
-  holiday: "Halloween",
+	'year' : 2012,
+	'month' : 'October',
+	'day' : 31,
+	'holiday' : 'Halloween'
 };
 
-var tinyAlmanac = {
-  year: 2012,
-  month: "October",
-  day: 31,
-  holiday: "Halloween",
-};
-
-var longString =
-  "Four score \
+var longString = "Four score \
 and seven years ago \
 our fathers brought forth \
 on this continent \
