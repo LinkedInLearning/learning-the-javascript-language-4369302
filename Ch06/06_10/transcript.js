411884144d5f8771340132f055c124dc912526dc
Joe Chellman <joe@shooflydesign.org>
2023-03-24 10:41:18 -0600

    Improved callback examples

--- Ch06/06_10/transcript.js
+++ Ch06/06_10/transcript.js
@@ -6,11 +6,19 @@ var myNumbers = [1, 2, 3, 4, 5];
 
 var myDoubles = myNumbers.map(doubleIt);
 
-myNumbers.forEach(function(number) {
+myNumbers.forEach(function (number) {
   console.log("My array contains", number);
 });
 
-doubleIt = number => (number *= 2);
+myNumbers.forEach((number) => {
+  console.log("My array contains", number);
+});
+
+// this is a browser-based example
+const myTextField = document.getElementById("myTextField");
+myTextField.addEventListener("keyup", () => {
+  console.log("Someone is typing!");
+});
 
 // https://developer.mozilla.org/en-US/docs/Glossary/Callback_function
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
