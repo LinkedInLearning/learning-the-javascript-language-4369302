89b8b53325346daf8ec7c5ea87740c97785062cd
VAMAN ACHARYA <vaman.acharya001@gmail.com>
2024-01-01 13:02:52 +0530

    Update transcript.js

--- Ch02/02_01/transcript.js
+++ Ch02/02_01/transcript.js
@@ -1,6 +1,13 @@
 // This is a transcript of the lines of code from the lesson.
 // You can copy and paste each line into a JavaScript console to execute it and see the result.
 
+// Naming variables
+    // Variable names are pretty flexible as long as you follow a few rules:
+        // Start them with a letter, underscore _, or dollar sign $.
+        // After the first letter, you can use numbers, as well as letters, underscores, or dollar signs.
+        // Don’t use any of JavaScript’s reserved keywords.
+
+
 var x = 32;
 x;
 
